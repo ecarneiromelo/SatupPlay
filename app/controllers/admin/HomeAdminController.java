3fc71a6b89e939815bb4e3920942eeb02de1c521
Edi <Edi@Edi-PC>
2017-04-13 08:36:18 -0300

    add select in home page

--- app/controllers/admin/HomeAdminController.java
+++ app/controllers/admin/HomeAdminController.java
@@ -2,12 +2,13 @@ package controllers.admin;
 
 import controllers.Check;
 import controllers.base.BaseAdminController;
+import controllers.base.BaseCRUDController;
 
 /**
  * @author jgomes | 8 de jul de 2016 - 21:51:23
  */
-@Check("/adm/home")
-public class HomeAdminController extends BaseAdminController {
+
+public class HomeAdminController extends BaseCRUDController {
 
     public static void index() {
         render();
