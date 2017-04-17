9a389614995ec73b64c73ed54026d5d7d7c0f6f8
Edi <Edi@Edi-PC>
2017-04-16 21:27:52 -0300

    alter

--- app/controllers/guest/HomeGuestController.java
+++
@@ -1,9 +0,0 @@
-package controllers.guest;
-
-import controllers.base.BaseGuestController;
-
-/**
- * @author jgomes | 8 de jul de 2016 - 21:52:28
- */
-public class HomeGuestController extends BaseGuestController {
-}
