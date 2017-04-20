2ab1514bb7d9cc6fff6999952d005d4bed5689c6
Edivaldo Melo <edimelo15@hotmail.com>
2017-04-20 19:26:25 -0300

    add login

--- app/controllers/admin/HomeAdminController.java
+++ app/controllers/admin/HomeAdminController.java
@@ -3,15 +3,15 @@ package controllers.admin;
 import java.util.ArrayList;
 import java.util.List;
 
-import controllers.base.BaseCRUDController;
 import models.LinhaBO;
 import models.OnibusBO;
+import models.base.BaseAdminController;
 import models.base.BaseModel;
 
 /**
  * @author jgomes | 8 de jul de 2016 - 21:51:23
  */
-public class HomeAdminController extends BaseCRUDController {
+public class HomeAdminController extends BaseAdminController {
 
     public static void index() {
         render();
