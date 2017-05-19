0fba1a0fb820d1e349c087119c3a4b0cdab28522
Edivaldo Melo <edimelo15@hotmail.com>
2017-05-19 17:12:12 -0300

    add init data

---
+++ db/db-init-data.sql
@@ -0,0 +1,7 @@
+--------------------------------------------------------
+--  Arquivo criado - maio-29-2017   
+--------------------------------------------------------
+
+INSERT INTO tb_user(
+            id, email, name, pass, login_attempts, status)
+    VALUES (0, ‘master’ ,’master’,md5(’master’) , 0, 0);
